c23775e1c083560047ddf2501f21be8f8c446fb5
Park Jae Seong <timer973@naver.com>
2025-03-12 17:21:37 +0900

    First fetch to Origin

---
+++ src/main/java/com/movie_ai_recommend/movie_ai_recommend/MovieAiRecommendApplication.java
@@ -0,0 +1,13 @@
+package com.movie_ai_recommend.movie_ai_recommend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieAiRecommendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieAiRecommendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/movie_ai_recommend/movie_ai_recommend/MovieAiRecommendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.movie_ai_recommend.movie_ai_recommend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieAiRecommendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
