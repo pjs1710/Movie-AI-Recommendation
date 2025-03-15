d83aa8058a9f5cb4c4cf3b580611ad04b7a49a95
Park Jae Seong <timer973@naver.com>
2025-03-15 16:23:15 +0900

    ⌨refact: GeminiController 예외 처리

--- src/main/java/com/movie_ai_recommend/movie_ai_recommend/controller/gemini/GeminiController.java
+++ src/main/java/com/movie_ai_recommend/movie_ai_recommend/controller/gemini/GeminiController.java
@@ -3,6 +3,7 @@ package com.movie_ai_recommend.movie_ai_recommend.controller.gemini;
 import com.movie_ai_recommend.movie_ai_recommend.dto.gemini.GeminiDto;
 import com.movie_ai_recommend.movie_ai_recommend.service.gemini.GeminiService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -24,8 +25,14 @@ public class GeminiController {
 
     @PostMapping("/ask")
     public ResponseEntity<?> askGemini(@RequestBody GeminiDto geminiDto) {
-        GeminiDto resonse = geminiService.getAnswer(geminiDto, geminiDto.getUserId());
-        return ResponseEntity.ok(resonse);
+        try {
+            GeminiDto response = geminiService.getAnswer(geminiDto, geminiDto.getUserId());
+            return ResponseEntity.ok(response);
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Gemini API 호출 실패: " + e.getMessage());
+        }
     }
 
 }
