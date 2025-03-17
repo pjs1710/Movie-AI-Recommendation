e4c91805d97ec8481d2aefd1ae15d96ef30a8d04
Park Jae Seong <timer973@naver.com>
2025-03-17 18:42:32 +0900

    ⭐feat: Login 기능 구현 및 렌더링 API 구현

--- src/main/java/com/movie_ai_recommend/movie_ai_recommend/repository/UserRepository.java
+++ src/main/java/com/movie_ai_recommend/movie_ai_recommend/repository/UserRepository.java
@@ -4,8 +4,12 @@ import com.movie_ai_recommend.movie_ai_recommend.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
     User findByUserName(String userName);
+
+    Optional<User> findByEmail(String email);
 }
---
+++ src/main/java/com/movie_ai_recommend/movie_ai_recommend/service/user/LoginServiceImpl.java
@@ -0,0 +1,30 @@
+package com.movie_ai_recommend.movie_ai_recommend.service.user;
+
+import com.movie_ai_recommend.movie_ai_recommend.dto.user.LoginDto;
+import com.movie_ai_recommend.movie_ai_recommend.entity.User;
+import com.movie_ai_recommend.movie_ai_recommend.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class LoginServiceImpl implements LoginService {
+
+    private final UserRepository userRepository;
+
+    /**
+     * 로그인 처리 구현부입니다.
+     * @param loginDto
+     */
+    @Override
+    public void login(LoginDto loginDto) {
+        User user = userRepository.findByEmail(loginDto.getEmail())
+                .orElse(null);
+        /*
+         보안을 위해 이메일 OR 비밀번호가 틀렸다는 메세지 전달
+        */
+        if (user == null || !user.getPassword().equals(loginDto.getPassword())) {
+            throw new IllegalArgumentException("이메일 또는 비밀번호가 일치하지 않습니다.");
+        }
+    }
+}
